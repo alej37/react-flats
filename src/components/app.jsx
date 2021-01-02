25df25fbce4aae53245631573bca3dd0d9eb4f47
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2021-01-02 15:39:04 -0500

    Finished but google map has errors

---
+++ src/components/app.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Flatlist from './flatlist';
+import flats from '../../data/flats';
+import Marker from './marker';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFlat: flats[0],
+      flats
+    };
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  selectFlat = (index) => {
+    this.setState({selectedFlat: flats[index]});
+  }
+
+  render() {
+    return (
+      <div>
+        <Flatlist
+          flats={this.state.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectflat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+            <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
